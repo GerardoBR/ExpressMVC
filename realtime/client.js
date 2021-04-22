6c3188e35190b63d59e815232ff376479685b006
Prodevelopers001 <gerardo_barron_rojas@outlook.es>
2021-04-22 16:17:46 -0500

    Cambie el host de la websockets

--- realtime/client.js
+++ realtime/client.js
@@ -1,6 +1,9 @@
 const io = require('socket.io-client');
-
-let socket = io.connect('http://localhost:3001',{reconnect :true});
+let host =  'http://localhost:3001';
+if(process.env.NODE_ENV && process.env.NODE_ENV =='production'){
+    host = 'https://calm-temple-54932.herokuapp.com/';
+  }
+let socket = io.connect(host,{reconnect :true});
 
 socket.on('connect',function(){
     console.log("\n\nSocket connected  from NodeJs\n\n");
